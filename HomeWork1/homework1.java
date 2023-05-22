00b118c75b1b48c7d0103bd7a5babc802097e955
AlexeyLopuxov <alexeylopyxov00@gmail.com>
2023-05-22 23:39:23 +0500

    homework1

---
+++ HomeWork1/homework1.java
@@ -0,0 +1,88 @@
+package HomeWork1;
+
+import java.util.Scanner;
+
+public class homework1 {
+    public static void main(String[] args) {
+        // Задача 1
+        // Вычислить n-ое треугольного число (сумма чисел от 1 до n)
+        // n! (произведение чисел от 1 до n)
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Введите число: ");
+        // int n = scanner.nextInt();
+
+        // int sum = 0;
+        // for (int i = 1; i <= n; i++) {
+        //     sum += i;
+        // }
+        // System.out.println("Сумма от 1 до n: " + sum);
+
+        // int prod = 1;
+        // for (int j = 1; j <= n; j++) {
+        //     prod *= j;
+        // }
+        // System.out.println("Произведение чисел от 1 до n: " + prod);
+
+
+
+
+
+        // Задача 2
+        // Вывести все простые числа от 1 до 1000
+        
+        // boolean bl = true;
+        // System.out.print("Простые числа от 1 до 1000: ");
+        // for (int i = 2; i <= 1000; i++) {
+        //     for (int j = 2; j < i; j++) {
+        //         if (i % j == 0) {
+        //             bl = false;
+        //         }
+        //     }
+        //     if (bl) System.out.print(i + " ");
+        //     else bl = true;
+        // }
+
+
+
+
+
+        // Задача 3
+        // Реализовать простой калькулятор
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Введите первое число: ");
+        // double num1 = scanner.nextDouble();
+        // System.out.print("Введите арифметическое действие: ");
+        // char str = scanner.next().charAt(0);
+        // System.out.print("Введите второе число: ");
+        // double num2 = scanner.nextDouble();
+
+        // if (str == '+') {
+        //     double sum = num1 + num2;
+        //     System.out.println(num1 + " + " + num2 + " = " + sum);
+        // }
+
+        // if (str == '-'){
+        //     double minus = num1 - num2;
+        //     System.out.println(num1 + " - " + num2 + " = " + minus);
+        // }
+
+        // if (str == '*'){
+        //     double prod = num1 * num2;
+        //     System.out.println(num1 + " * " + num2 + " = " + prod);
+        // }
+
+        // if (str == '/'){
+        //     double del = num1 / num2;
+        //     System.out.println(num1 + " / " + num2 + " = " + del);
+        // }
+
+
+
+
+
+        // Задача 4
+        // 
+    }
+}
